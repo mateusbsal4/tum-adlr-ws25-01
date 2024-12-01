9d627ac50a5be9cd0c03b184383567fc0ea0ce08
Mateus B. Salomao <mateus.salomao4@usp.br>
2024-12-01 23:12:40 +0000

    PPO for MuJoCo environments

---
+++ PPO/stable_baselines/ppo_mujoco_envs.py
@@ -0,0 +1,20 @@
+import os
+os.environ['MUJOCO_GL'] = 'egl'
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from render_browser import render_browser
+
+@render_browser
+def test_policy():
+    #vec_env = make_vec_env("Walker2d-v5", n_envs=1, env_kwargs={"render_mode": "rgb_array"})   #2d walker 
+    vec_env = make_vec_env("Ant-v5", n_envs=1, env_kwargs={"render_mode": "rgb_array"}) 
+    model = PPO("MlpPolicy", vec_env, verbose=1, device = "cuda:0")
+    model.learn(total_timesteps=25000)
+    obs = vec_env.reset()
+    while True:
+        action, _ = model.predict(obs)
+        obs, _, _, _ = vec_env.step(action)
+        yield vec_env.render(mode="rgb_array")
+
+test_policy()
